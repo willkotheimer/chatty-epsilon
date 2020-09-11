1d705661bba53f1dd6b88ddde579a78141a57ceb
wkotheimer <wkotheimer@gmail.com>
2020-09-10 22:34:14 -0500

    Added print to dom function

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import navbar from './components/navbar';
+import Printmessages from './components/printMessages';
 
 import '../styles/main.scss';
 
 const init = () => {
   navbar.buildNavbar();
+  Printmessages.addmessages();
 };
 
 init();
